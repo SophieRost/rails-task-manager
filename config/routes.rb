2c25d8ced50e3606e97266ed535cf916ad314826
Sophie Rost <laram.risgen@gmail.com>
2018-11-13 15:32:36 +0100

    Ex 2 ok optional part to do

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # READ
+  get '/tasks', to: 'tasks#index', as: :index
+  # CREATE
+  get '/tasks/new', to: 'tasks#new', as: :new
+  post '/tasks', to: 'tasks#create'
+  # READ
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # UPDATE
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  # DELETE
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
